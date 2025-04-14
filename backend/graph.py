9e043a7ca573a1c48fa447f6061ce408c924dc48
Sahil <sahil@Sahils-MacBook-Air.local>
2025-04-14 17:38:27 +0400

    Major Updates

--- backend/graph.py
+++ backend/graph.py
@@ -5,26 +5,18 @@ class Graph:
         self.nodes = {}
         self.adjacency_list = {}
         self.load_layout(layout_file)
-        
+
     def load_layout(self, layout_file):
-        #Loading building layout from the JSON file and initlalsing nodes and edges
-        
         with open(layout_file, 'r') as f:
             data = json.load(f)
-            
-        #start with the exit status and prepare for empty neighbour list
+
         for node in data['nodes']:
-            node_id = node['id']
-            self.nodes[node_id] = node
-            self.adjacency_list[node_id] = [] #initialise and empty list for neighbour
-            
+            self.nodes[node['id']] = node
+            self.adjacency_list[node['id']] = []
+
         for edge in data['edges']:
-            from_node = edge['from']
-            to_node = edge['to']
-            self.adjacency_list[from_node].append(to_node)
-            self.adjacency_list[to_node].append(from_node)
+            self.adjacency_list[edge['from']].append(edge['to'])
+            self.adjacency_list[edge['to']].append(edge['from'])
 
     def is_exit(self, node_id):
-        #Check if the node is exit
-        
         return self.nodes[node_id].get('exit', False)
